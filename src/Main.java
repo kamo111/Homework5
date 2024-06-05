2c9dc0483ae23b852b1ccd185993cf9e1ad32ebd
kamo111 <kamo19.989king@gmail.com>
2024-06-05 14:33:14 +0300

    HW-5 done fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -17,20 +17,17 @@ public class Main {
         System.out.println("Задание 2.");
 
         int clientDeviceYear = 2015;
-        if (clientOS == 0) {
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            }
-            else
-                System.out.println("Установите версию приложения для iOS по ссылке.");
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
         }
-        else if (clientOS == 1) {
-
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            }
-            else
-                System.out.println("Установите версию приложения для Android по ссылке.");
+        else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }
+        else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+        else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
         }
         else {
             System.out.println("Неопознаная операционная система.");
@@ -38,39 +35,29 @@ public class Main {
         //Задание 3
         System.out.println("Задание 3.");
 
-        int year = 2021;
-        boolean leapYear = false;
+        int year = 2024;
         if (year > 1584) {
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                        leapYear = true;
+             if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println("Високосный год.");
                 }
             else {
-                leapYear = false;
+                System.out.println("Невисокосный год.");
             }
         }
-        if (!leapYear) {
-            System.out.println("Невисокосный год.");
-        }
-        else{
-                System.out.println("Bисокосный год.");
-            }
+
         //Задание 4
         System.out.println("Задание 4.");
 
-        int deliveryDistance = 95;
-        int day = 0;
-                if (deliveryDistance == 20) {
-                    day =  1;
-                    System.out.println("Потребуется дней: " + day);
+        int deliveryDistance = 20;
+                if (deliveryDistance > 0 && deliveryDistance < 20) {
+                    System.out.println("Потребуется день доставки.");
                 }
-                else if (deliveryDistance > 20 && deliveryDistance < 60 ) {
-                    day = 2;
-                    System.out.println("Потребуется дней: " + day);
+                else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+                    System.out.println("Потребуется 2 дня доставки.");
                 }
 
                 else if (deliveryDistance > 60 && deliveryDistance < 100 ) {
-                    day = 3;
-                    System.out.println("Потребуется дней: " + day);
+                    System.out.println("Потребуется 3 дня доставки.");
                 }
                 else
                 System.out.println("Доставки нет.");
